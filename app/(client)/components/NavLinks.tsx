81c28389074865dde3e9584c6f959f668844aca9
mehrab esmailnia <mehrabesmailnia96@gmail.com>
2023-11-26 02:11:17 +0330

    remove underline links

--- app/(client)/components/NavLinks.tsx
+++ app/(client)/components/NavLinks.tsx
@@ -8,10 +8,10 @@ const NavLinks = () => {
 
   return (
     <nav className='py-4 relative rounded-3xl px-8 bg-slate-100 flex justify-between'>
-        <Link className={`link ${currentPass == '/' ? 'activeLink' : ''}`} href="/">About me</Link>
-        <Link className={`link ${currentPass == '/resume' ? 'activeLink' : ''}`} href="/resume">Resume</Link>
-        <Link className={`link ${currentPass == '/work-samples' ? 'activeLink' : ''}`} href="/work-samples">Work Samples</Link>
-        <Link className={`link ${currentPass == '/contact-me' ? 'activeLink' : ''}`} href="/contact-me">Contact me</Link>
+        <Link className={`link  no-underline ${currentPass == '/' ? 'activeLink' : ''}`} href="/">About me</Link>
+        <Link className={`link  no-underline ${currentPass == '/resume' ? 'activeLink' : ''}`} href="/resume">Resume</Link>
+        <Link className={`link  no-underline ${currentPass == '/work-samples' ? 'activeLink' : ''}`} href="/work-samples">Work Samples</Link>
+        <Link className={`link  no-underline ${currentPass == '/contact-me' ? 'activeLink' : ''}`} href="/contact-me">Contact me</Link>
     </nav>
   )
 }
